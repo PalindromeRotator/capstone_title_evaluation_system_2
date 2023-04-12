1b15408da7c4627925cc4b123af56449e8e0a421
Mikeyyy0119 <villarta1999@gmail.com>
2023-04-13 03:00:36 +0800

    13/04/2023
    Capstone Group

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -11,7 +11,7 @@ export const ROUTES: RouteInfo[] = [
     { path: '/user-profile', title: 'Profile',  icon:'ni-single-02 text-yellow', class: '' },
     { path: '/tables', title: 'AM Admin',  icon:'ni-bullet-list-67 text-red', class: '' },
     { path: '/dashboard', title: 'Admin Dashboard',  icon: 'ni-tv-2 text-primary', class: '' },
-    { path: '/maps', title: 'Working',  icon:'ni-pin-3 text-orange', class: '' },
+    { path: '/maps', title: 'CapstoneGroup',  icon:'ni-pin-3 text-orange', class: '' },
     { path: '/icons', title: 'Working',  icon:'ni-planet text-blue', class: '' },
     { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' },
     { path: '/registerOptions', title: 'Register',  icon:'ni-circle-08 text-pink', class: '' }
