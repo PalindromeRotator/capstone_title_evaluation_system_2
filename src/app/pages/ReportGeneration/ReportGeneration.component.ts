8cc28364d0617a22c27019e5bfa79905c95eed45
Kenneth Galvez <57065417+PalindromeRotator@users.noreply.github.com>
2023-04-30 10:48:00 +0800

    last

--- src/app/pages/ReportGeneration/ReportGeneration.component.ts
+++ src/app/pages/ReportGeneration/ReportGeneration.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
-
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { Titles } from 'src/app/models/titles';
+import { TitlesService } from 'src/app/services/titles.service';
+import Swal from 'sweetalert2';
+import * as XLSX from 'xlsx';
 
 
 
@@ -13,12 +16,86 @@ import {NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 export class ReportGeneration implements OnInit {
 
-  constructor() { }
+  constructor(private titlesService: TitlesService) { }
+  from!: NgbDateStruct;
+  into!: NgbDateStruct;
 
-  ngOnInit() { }
-  
+  filter = {
+    from: '',
+    into: '',
+    selectedFilter: 'none',
+  }
+  titlesData!: Array<Titles>
+  ngOnInit() {
+
+    this.titlesService.getAll().subscribe(
+      response => {
+
+      }
+    )
+  }
+
+  findFilter(): void {
+    const from = this.getSelectedDate(this.filter.from);
+    const into = this.getSelectedDate(this.filter.into);
+
+    const epochTimeFrom = from.getTime();
+    const epochTimeInto = into.getTime();
+
+    if (this.filter.from === '' || this.filter.from === '') {
+      Swal.fire({
+        icon: 'error',
+        text: 'Date filter form cannot be empty.'
+      })
+    }
+    else {
+      var result: Array<Object> = [
+      ]
+      this.titlesService.getAll().subscribe(
+        response => {
+
+          for (let data of response) {
+            var dataCreatedDate = new Date(data.createdAt!);
+            if (dataCreatedDate.getTime() >= epochTimeFrom && dataCreatedDate.getTime() <= epochTimeInto) {
+              result.push(data)
+            }
+          }
+
+          this.titlesData = []
+          console.log(result)
+          this.titlesData = result
+        }
+      )
+    }
+  }
+
+  getSelectedDate(from: string): Date {
+    return new Date(from);
+  }
+
+  exportexcel(filename: string, element_name: string): void {
+    Swal.fire({
+      icon: 'success',
+      title: 'Done',
+      text: 'Successfully download a report sheet',
+    }).then((result) => {
+      let element = document.getElementById(element_name);
+      const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
+      /* generate workbook and add the worksheet */
+      const wb: XLSX.WorkBook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+      /* save to file */
+      XLSX.writeFile(wb, `${filename}.xlsx`);
+    })
+    /* pass here the table id */
+
+
+  }
+
   displayStyle = "none";
-  
+
   openPopup() {
     this.displayStyle = "block";
   }
